9c8dd71c091b785d186d9afb73f28b5f67ba0dce
datlowashere <datlowashere>
2023-09-20 21:59:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const express = require("express");
+const express = require('express');
+const bodyParser=require('body-parser');
 const app = express();
 const port = 3000;
 
+const route=require('./src/routes/index');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(bodyParser.json({limit: '50mb'}));
+
+
+route(app);
 
 
 app.listen(port, () => {
